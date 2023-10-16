d13bf8fa4de6afd129bc9332702a3b10d4209c77
Kirill Skladanov <wra1th@inbox.ru>
2023-10-16 20:00:00 +0300

    <feat> подготовить дто для статистики

---
+++ ewm-stats-service/stats-dto/src/main/java/AllStatsDto.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*@Data
+@NoArgsConstructor
+@Builder*/
+@AllArgsConstructor
+public class AllStatsDto {
+    private String app;
+    private String uri;
+    private long hits;
+}
---
+++ ewm-stats-service/stats-dto/src/main/java/HitDto.java
@@ -0,0 +1,24 @@
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+/*@Data
+@NoArgsConstructor*/
+@AllArgsConstructor
+public class HitDto {
+    private Long id;
+    @NotBlank(message = "Не указано название приложения")
+    private String app;
+    @NotBlank(message = "Не указан URL")
+    private String uri;
+    @NotBlank(message = "Не указан IP")
+    private String ip;
+    @NotNull(message = "Не указано время")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
