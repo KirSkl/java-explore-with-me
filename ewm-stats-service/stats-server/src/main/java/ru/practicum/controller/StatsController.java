ec84a5958045605db9f422e1cb88ec95ae1777ce
Kirill Skladanov <wra1th@inbox.ru>
2023-10-19 17:47:38 +0300

    <fix>

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -2,9 +2,10 @@ package ru.practicum.controller;
 
 import dto.EndpointHitDto;
 import dto.ViewStatsDto;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.service.StatsService;
 
@@ -14,20 +15,29 @@ import java.util.List;
 
 @RestController
 @Slf4j
-@AllArgsConstructor
 public class StatsController {
     private final StatsService statsService;
     private final String timePattern = "yyyy-MM-dd HH:mm:ss";
 
+    @Autowired
+    public StatsController(StatsService statsService) {
+        this.statsService = statsService;
+    }
+
     @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public void createHit(@RequestBody EndpointHitDto endpointHitDto) {
         log.info("Получен запрос POST /hit");
         statsService.createHit(endpointHitDto);
     }
 
     @GetMapping("/stats")
-    public List<ViewStatsDto> getStats(@RequestParam @DateTimeFormat(pattern = timePattern) LocalDateTime start, @RequestParam @DateTimeFormat(pattern = timePattern) LocalDateTime end, @RequestParam(defaultValue = "false") Boolean unique, @RequestParam(required = false) List<String> uris) {
-        log.info(String.format("Получен запрос GET /stats с параметрами: start = %s, end = %s, unique = %s, uris = %s", start, end, unique, uris));
+    public List<ViewStatsDto> getStats(@RequestParam @DateTimeFormat(pattern = timePattern) LocalDateTime start,
+                                       @RequestParam @DateTimeFormat(pattern = timePattern) LocalDateTime end,
+                                       @RequestParam(defaultValue = "false") Boolean unique,
+                                       @RequestParam(required = false) List<String> uris) {
+        log.info(String.format("Получен запрос GET /stats с параметрами: start = %s, end = %s, unique = %s, uris = %s",
+                start, end, unique, uris));
         List<String> urisList = uris != null ? uris : new ArrayList<>();
         return statsService.getStats(start, end, unique, urisList);
     }
