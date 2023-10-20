4af5fa4036aa15bb0606dac696fe53f47a4875fd
Kirill Skladanov <wra1th@inbox.ru>
2023-10-20 17:20:58 +0300

    <fix> изменить запросные методы на jpql для переноса логики из сервисного в слоя в субд

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -21,22 +21,24 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     List<EndpointHit> getDistinctFirstByUriInAndTimestampBetween(List<String> uris, LocalDateTime start,
                                                                  LocalDateTime end);
 
-    @Query(value = "SELECT new dto.ViewStatsDto(h.app, h.uri, count(h.ip)) " +
-            "FROM EndpointHit as h " +
+    @Query(value = "SELECT new dto.ViewStatsDto(h.app, h.uri, COUNT(h.ip)) " +
+            "FROM EndpointHit AS h " +
             "WHERE h.timestamp BETWEEN ?1 AND ?2 " +
-            "GROUP BY h.uri, h.app")
+            "GROUP BY h.uri, h.app " +
+            "ORDER BY COUNT(h.ip) DESC")
     List<ViewStatsDto> getAllStats(LocalDateTime start, LocalDateTime end);
 
-    @Query(value = "SELECT new dto.ViewStatsDto(h.app, h.uri, count(DISTINCT h.ip)) " +
-            "FROM EndpointHit as h " +
+    @Query(value = "SELECT new dto.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
+            "FROM EndpointHit AS h " +
             "WHERE h.timestamp BETWEEN ?1 AND ?2 " +
-            "GROUP BY h.uri, h.app")
+            "GROUP BY h.uri, h.app " +
+            "ORDER BY COUNT(h.ip) DESC")
     List<ViewStatsDto> getAllStatsUnique(LocalDateTime start, LocalDateTime end);
 
-    @Query(value = "SELECT new dto.ViewStatsDto(h.app, h.uri, count(h.ip)) " +
-            "FROM EndpointHit as h " +
+    @Query(value = "SELECT new dto.ViewStatsDto(h.app, h.uri, COUNT(h.ip)) " +
+            "FROM EndpointHit AS h " +
             "WHERE h.uri IN (?1) AND h.timestamp BETWEEN ?2 AND ?3 " +
             "GROUP BY h.uri, h.app " +
-            "ORDER BY count(h.ip) DESC")
+            "ORDER BY COUNT(h.ip) DESC")
     List<ViewStatsDto> getAllStatsByUri(Iterable uris, LocalDateTime start, LocalDateTime end);
 }
