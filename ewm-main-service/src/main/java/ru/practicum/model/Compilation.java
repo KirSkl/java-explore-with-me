bd947bacbf9b8cf18824650981e9aff7e141c96d
Kirill Skladanov <wra1th@inbox.ru>
2023-11-09 18:06:05 +0300

    <fix>

--- ewm-main-service/src/main/java/ru/practicum/model/Compilation.java
+++ ewm-main-service/src/main/java/ru/practicum/model/Compilation.java
@@ -12,7 +12,6 @@ import java.util.Objects;
 @NoArgsConstructor
 @Getter
 @Setter
-@RequiredArgsConstructor
 public class Compilation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
