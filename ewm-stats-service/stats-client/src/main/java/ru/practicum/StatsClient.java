75447d48902191d8cdd6467555dac102ad686c3a
Kirill Skladanov <wra1th@inbox.ru>
2023-11-21 01:35:08 +0300

    <fix > удаление чекстайла

--- ewm-stats-service/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ ewm-stats-service/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -25,7 +25,7 @@ public class StatsClient {
     private final RestTemplate rest;
 
     @Autowired
-    public StatsClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
+    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
         rest = builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
