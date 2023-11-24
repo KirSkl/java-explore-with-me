2a97de8ac695ffe29086d9212e0c3a079deac222
Kirill Skladanov <wra1th@inbox.ru>
2023-11-24 17:08:13 +0300

    <fix > решение проблемы со стастистикой

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -41,7 +41,7 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Modifying
     @Query(value = "SELECT new dto.ViewStatsDto(h.app, h.uri, COUNT(h.ip)) " +
             "FROM EndpointHit AS h " +
-            "WHERE h.uri IN :uris AND h.timestamp BETWEEN :start AND :end " +
+            "WHERE :uris like concat('%', h.uri, '%') AND h.timestamp BETWEEN :start AND :end " +
             "GROUP BY h.uri, h.app " +
             "ORDER BY COUNT(h.ip) DESC")
     List<ViewStatsDto> getAllStatsByUri(List<String> uris, LocalDateTime start, LocalDateTime end);
@@ -49,7 +49,7 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Modifying
     @Query(value = "SELECT new dto.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM EndpointHit AS h " +
-            "WHERE concat('[', h.uri, ']') IN :uris AND h.timestamp BETWEEN :start and :end " +
+            "WHERE :uris like concat('%', h.uri, '%') AND h.timestamp BETWEEN :start and :end " +
             "GROUP BY h.uri, h.app " +
             "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStatsDto> getAllStatsByUriUnique(List<String> uris, LocalDateTime start, LocalDateTime end);
